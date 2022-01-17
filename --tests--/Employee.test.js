e5bbe9af93e94177621babeaad9b4f9213ae9d51
Skoobieton <91810317+Tanyaskubon@users.noreply.github.com>
2022-01-17 15:44:02 -0500

    add test

--- --tests--/Employee.test.js
+++ --tests--/Employee.test.js
@@ -1,10 +1,33 @@
 const Employee = require ('../lib/Employee')
 
-test(" match employee",() => {
-    const employee = new Employee ('name',12,'email');
+test(" can create Enmployee",() => {
+    const em = new Employee ('Tanya', 12, 'tanyaskubon25@gmail.com');
   
-    // test for success match
-    expect(employee.getName()).toMatch('name');
-  
-    
-  })
+    expect(typeof(em)).toBe('object');
+   
+  });
+
+  test(" gets employee name", () => {
+   const em = new Employee('Tanya', 12, 'tanyaskubon25@gmail.com');
+   
+   expect(em.getName()).toEqual(expect.any(String));
+  });
+
+  test("gets employee id", () =>  {
+    const em = new Employee('Tanya', 12, 'tanyaskubon25@gmail.com');
+   
+    expect(em.getId()).toEqual(expect.any(Number));
+  });
+
+test('gets employee email', () => {
+  const em = new Employee('Tanya', 12, 'tanyaskubon25@gmail.com');
+
+  expect(em.getEmail()).toEqual(expect.stringContaining(em.email.toString()));
+});
+
+
+test('gets role of employee', () => {
+  const em = new Employee('Tanya', 12, 'tanyaskubon25@gmail.com');
+
+  expect(em.getRole()).toEqual("Employee");
+}); 
